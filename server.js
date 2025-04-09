549ac404e679bbac0496b56c2294ead20ff2c26b
Daniel <danielholderik@gmail.com>
2025-04-09 17:41:47 +0200

    Update server.js

--- server.js
+++ server.js
@@ -21,9 +21,50 @@ mongoose.connect(CONNECTION_URL,{useNewUrlParser: true, useUnifiedTopology: true
     console.error("connection to DB failed!", err);
 });
 
-//API stuff
-server.get('api/dishes', function async (req, res){
+//----API stuff---
 
+//get all recipes
+server.get('api/dishes', async function (req, res){
+try{
+    const allRecipes = await recipe.find();
+    res.status(200).json(allRecipes);
+}
+catch(err){
+    res.status(500).json({message: err.message});
+}
+});
+
+//get recipe by name
+server.get('/api/dishes/:name', async function (req, res){
+    try{
+        const recipeName = req.params.name;
+        const recipeFound = await recipe.findOne({name: recipeName});
+        if(recipeFound){
+            res.status(200).json(recipeFound);
+        }else{
+            res.status(404).json({message: 'Recipe not found'});
+        }
+    }
+    catch(err){
+        res.status(500).json({message: err.message});
+    }
+});
+
+//post new recipe
+server.post('/api/dishes', async function (req, res){
+try{
+    const existingRecipe = await recipe.findOne({name: req.body.name});
+    
+    if(!existingRecipe){
+        const newRecipe = new recipe(req.body);
+        await newRecipe.save();
+        res.status(201).json(newRecipe);
+    }else{
+        res.status(409).json({message: 'dish exists'});
+    }
+}catch(err){
+    res.status(500).json({message: err.message});
+}
 });
 
 
