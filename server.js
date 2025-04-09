3ca35380c51bd7d7fbc67615edf21047d04a513f
Daniel <danielholderik@gmail.com>
2025-04-09 19:39:48 +0200

    finished server.js and updated err messages

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const bodyParser = require('body-parser');
-const recipe = require('./database/database.js');
+const Recipe = require('./database/database.js');
 
 //innitialize the app
 const server = express();
@@ -24,13 +24,13 @@ mongoose.connect(CONNECTION_URL,{useNewUrlParser: true, useUnifiedTopology: true
 //----API stuff---
 
 //get all recipes
-server.get('api/dishes', async function (req, res){
+server.get('/api/dishes', async function (res){
 try{
-    const allRecipes = await recipe.find();
+    const allRecipes = await Recipe.find();
     res.status(200).json(allRecipes);
 }
 catch(err){
-    res.status(500).json({message: err.message});
+    res.status(500).json({message: "error retrieving recipes" + err.message});
 }
 });
 
@@ -38,7 +38,7 @@ catch(err){
 server.get('/api/dishes/:name', async function (req, res){
     try{
         const recipeName = req.params.name;
-        const recipeFound = await recipe.findOne({name: recipeName});
+        const recipeFound = await Recipe.findOne({name: recipeName});
         if(recipeFound){
             res.status(200).json(recipeFound);
         }else{
@@ -46,14 +46,50 @@ server.get('/api/dishes/:name', async function (req, res){
         }
     }
     catch(err){
-        res.status(500).json({message: err.message});
+        res.status(500).json({message: "error getting recipe by name" + err.message});
     }
 });
+
+//uopdate dish
+server.put('/api/dishes/:id', async function (req, res){
+    const recipeId = req.params.id;
+
+    try {
+       const updatedRecipe = await Recipe.findByIdAndUpdate(
+        recipeId,
+         req.body,
+         {new: true}); 
+
+         if (!  updatedRecipe) {
+            return res.status(404).json({ message: 'recipe doesnt exist' });
+        }
+    } 
+    catch (err){
+        res.status(500).json({ message: "error in updating dish" + err.message });
+    }
+});
+
+//delete recipe
+server.delete('/api/dishes/:id', async function (req, res){
+    const recipeId = req.params.id;
+
+    try{
+        const recipeToDelete = await Recipe.findByIdAndDelete(recipeId);
+        if (!recipeToDelete){
+            return res.status(404).json({ message: 'recipe not found' + recipeId });
+        } 
+        res.status(200).json({ message: 'recipe deleted successfully' });
+    } 
+    catch (err){
+        res.status(500).json({ message: 'error deleting recipe' + err.message });
+    }
+});
+    
 
 //post new recipe
 server.post('/api/dishes', async function (req, res){
 try{
-    const existingRecipe = await recipe.findOne({name: req.body.name});
+    const existingRecipe = await Recipe.findOne({name: req.body.name});
     
     if(!existingRecipe){
         const newRecipe = new recipe(req.body);
@@ -62,8 +98,9 @@ try{
     }else{
         res.status(409).json({message: 'dish exists'});
     }
-}catch(err){
-    res.status(500).json({message: err.message});
+}
+catch(err){
+    res.status(500).json({message: "error adding new recipe" + err.message});
 }
 });
 
